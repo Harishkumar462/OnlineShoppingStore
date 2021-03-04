8417b0e0305446cf1457961381912f3c07a0c911
Harish <harishkumarskv@gmail.com>
2021-03-04 19:55:04 +0530

    OnlineShopping

---
+++ OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "harishkumarskv@gmail.com";
+        public string MailFromAddress = "harishkumarskv@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "harishkumarskv@gmail.com";
+        public string Password = ""; // Create your own google app password, In the video I have shown you how to create app password.
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
